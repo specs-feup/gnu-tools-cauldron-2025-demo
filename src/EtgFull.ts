615d469409d4bffb67a2a663d904ee6948952a34
tiagolascasas <tiagolascasas@outlook.com>
2025-05-02 01:43:02 +0100

    Adds ETG scenarios

---
+++ src/EtgFull.ts
@@ -0,0 +1,23 @@
+import { ExtendedTaskGraphAPI } from "@specs-feup/extended-task-graph/ExtendedTaskGraphAPI";
+import { GenFlowConfig } from "@specs-feup/extended-task-graph/GenFlowConfig";
+import { SubsetTransform } from "@specs-feup/extended-task-graph/SubsetTransforms";
+import { TransFlowConfig } from "@specs-feup/extended-task-graph/TransFlowConfig";
+
+const topFunctionName = "edge_detect";
+const outputDir = "outputs";
+const appName = "edgedetect-transflow";
+const api = new ExtendedTaskGraphAPI(topFunctionName, outputDir, appName);
+
+// Run code transformation flow
+const config1 = new TransFlowConfig();
+config1.transformRecipe = [
+    SubsetTransform.ArrayFlattener,
+    SubsetTransform.ConstantFoldingPropagation,
+    SubsetTransform.StructDecomposition,
+    SubsetTransform.ConstantFoldingPropagation
+];
+api.runCodeTransformationFlow(config1);
+
+// Run ETG generation flow
+const config2 = new GenFlowConfig();
+const etg = api.runTaskGraphGenerationFlow(config2);
---
+++ src/EtgTransforms.ts
@@ -0,0 +1,18 @@
+import { ExtendedTaskGraphAPI } from "@specs-feup/extended-task-graph/ExtendedTaskGraphAPI";
+import { SubsetTransform } from "@specs-feup/extended-task-graph/SubsetTransforms";
+import { TransFlowConfig } from "@specs-feup/extended-task-graph/TransFlowConfig";
+
+const topFunctionName = "edge_detect";
+const outputDir = "outputs";
+const appName = "edgedetect-transflow";
+const api = new ExtendedTaskGraphAPI(topFunctionName, outputDir, appName);
+
+// Run code transformation flow
+const config = new TransFlowConfig();
+config.transformRecipe = [
+    SubsetTransform.ArrayFlattener,
+    SubsetTransform.ConstantFoldingPropagation,
+    SubsetTransform.StructDecomposition,
+    SubsetTransform.ConstantFoldingPropagation
+];
+api.runCodeTransformationFlow(config);
